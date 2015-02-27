30ad7bed493b54ee485c0ca6e2cc74129bebc30d
Zhiyi Sun <zhiyisun@gmail.com>
2015-02-26 20:38:37 -0500

    remove local bin in path.zsh

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,7 +1,2 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-#Add local bin
-if [[ -f $HOME/bin ]]; then
-	export PATH="$HOME/bin:$PATH"
-fi
