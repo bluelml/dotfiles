999d795cd52104e527a8340ad21b6e15e10e16d4
Zhiyi Sun <zhiyisun@gmail.com>
2015-02-27 00:01:23 -0500

    Fixed path issue.

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,8 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$PATH:/usr/local/bin:/usr/local/sbin:$ZSH/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# put ~/bin on PATH if you have it
+if [[ -d $HOME/bin ]]; then
+	PATH="$HOME/bin:$PATH"
+fi
+export PATH="./bin:$PATH"
